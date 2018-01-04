9299bfc903f171d7fa86bda3d8371982e688405e
Robert Adamyan <robert.adamyan1@gmail.com>
2018-01-04 18:22:57 +0400

    add: initial working version

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="blah">{{ header }}</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      header: 'The build time is ridiculous. HMR is fast.'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './index';
+</style>
